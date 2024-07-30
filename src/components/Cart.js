60ed5cf345f0e19afe92aa52f2db7efefa7a113d
adenugbamichael <adenugbamicke@gmail.com>
2024-07-30 09:11:28 +0100

    store orders on the server

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -16,6 +16,7 @@ function Cart({ cart, dispatch, items }) {
   const [phone, setPhone] = useState("");
   const [zipCode, setZipCode] = useState("");
   const [isEmployeeOfTheMonth, setIsEmployeeOfTheMonth] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const debounceRef = useRef(null);
   const zipRef = useRef(null);
 
@@ -33,9 +34,22 @@ function Cart({ cart, dispatch, items }) {
   const total = subTotal + tax;
   const isFormValid = zipCode.length === 5 && name.trim();
 
-  const submitOrder = (event) => {
+  const submitOrder = async (event) => {
     event.preventDefault();
-    // TODO
+    setIsSubmitting(true);
+    try {
+      await axios.post("/api/orders", {
+        items: cart,
+        name,
+        phone,
+        zipCode,
+      });
+      console.log("Order Submitted");
+    } catch (error) {
+      console.error("Error submitting the order", error);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   const setFormattedPhone = (newNumber) => {
@@ -142,7 +156,7 @@ function Cart({ cart, dispatch, items }) {
                 ref={zipRef}
               />
             </label>
-            <button type="submit" disabled={!isFormValid}>
+            <button type="submit" disabled={!isFormValid || isSubmitting}>
               Order Now
             </button>
           </form>
